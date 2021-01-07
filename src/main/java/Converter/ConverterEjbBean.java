6b77aed7e1c2ce482464beb2d3358fff91f9b320
LaunoisRemy <remy.launois@gmail.com>
2021-01-07 16:34:05 +0100

    Add old method

--- src/main/java/Converter/ConverterEjbBean.java
+++ src/main/java/Converter/ConverterEjbBean.java
@@ -8,6 +8,7 @@ import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Stateless(name = "ConverterEjbEJB")
 public class ConverterEjbBean implements IConverter{
@@ -28,18 +29,36 @@ public class ConverterEjbBean implements IConverter{
         SAXBuilder sxb = new SAXBuilder();
 
         try{
-            URL url = new URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
-            HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
-            Document document = sxb . build (con.getInputStream());
-            Element racine = document.getRootElement ( ) ;
-            Namespace ns = Namespace.getNamespace (
-                    "http://www.ecb.int/vocabulary/2002-08-01/eurofxref") ;
-            List<Element> elem = racine.getChild ( "Cube" , ns ).getChild("Cube",ns).getChildren();
+            connecToSite(sxb);
+            List<Element> elem = connecToSite(sxb);
             elem.forEach(e -> codeToRate.put(e.getAttributeValue("currency"), Double.parseDouble(e.getAttributeValue("rate"))));
 
         } catch (IOException | JDOMException e) {
             e.printStackTrace();
         }
         return codeToRate;
+    }
+
+    public List<String> getAllCodeCurrency(){
+        SAXBuilder sxb = new SAXBuilder();
+        List<String> res = new ArrayList<>();
+        try{
+            ;
+            List<Element> elem = connecToSite(sxb);
+            res = elem.stream().map( e ->e.getAttributeValue("currency")).collect(Collectors.toList());
+        } catch (IOException | JDOMException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    private List<Element> connecToSite(SAXBuilder sxb) throws IOException, JDOMException {
+        URL url = new URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+        HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
+        Document document = sxb . build (con.getInputStream());
+        Element racine = document.getRootElement ( ) ;
+        Namespace ns = Namespace.getNamespace (
+                "http://www.ecb.int/vocabulary/2002-08-01/eurofxref") ;
+        return racine.getChild ( "Cube" , ns ).getChild("Cube",ns).getChildren();
     }
 }
